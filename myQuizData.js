444134b6c88634186104ab15f8c045b6680f5592
Thelaw818 <93685225+Thelaw818@users.noreply.github.com>
2025-01-11 19:11:40 -0800

    Create myQuizData.js

---
+++ myQuizData.js
@@ -0,0 +1,19 @@
+const quizData = [
+    {
+        question: "What year did the American Revolutionary War end?",
+        options: ["1776", "1781", "1783", "1801"],
+        answer: "1783",
+        type: "single-answer",
+    },
+    {
+        question: "Which of the following are fruits? Check all that apply.",
+        options: ["Apple", "Carrot", "Banana", "Potato"],
+        answer: ["Apple", "Banana"],
+        type: "multiple-answer",
+    },
+    {
+        question: "How many continents are there on Earth?",
+        answer: "7",
+        type: "free-form",
+    },
+];
